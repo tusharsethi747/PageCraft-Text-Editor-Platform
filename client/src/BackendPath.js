232891b595d9d5755ab50b81c3e941844fcdb013
Tushar <tusharsethi747@gmail.com>
2024-06-15 09:54:51 +0530

    removed / from the path

--- client/src/BackendPath.js
+++ client/src/BackendPath.js
@@ -1,3 +1,3 @@
 // const BackendPath=`http://localhost:5000`;
-const BackendPath=`https://page-craft-seven.vercel.app/`;
+const BackendPath=`https://page-craft-seven.vercel.app`;
 export default BackendPath
--- client/src/SocketPath.js
+++ client/src/SocketPath.js
@@ -1,3 +1,3 @@
 // const SocketPath=`http://localhost:5000/`;
-const SocketPath=`https://page-craft-seven.vercel.app/`;
+const SocketPath=`https://page-craft-seven.vercel.app`;
 export default SocketPath;
